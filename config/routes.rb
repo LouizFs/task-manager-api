435ffa02e1aba4e479635c127732b946157689bf
Luiz Felipe Silva <Luizfelipesilva23@hotmail.com>
2019-08-22 20:09:44 +0000

    Adding api version constraints to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
+require 'api_version_constraint'
+
 Rails.application.routes.draw do
   
-  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: "/" do
+  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: "/" do  
     
-  end
+    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default:true) do
 
+    end
+   
+    # namespace :v2, path: "/", constraints: ApiVersionConstraint.new(version: 2, default: true) do
+    #   resources tasks
+    # end
+
+  end
 end
+ 
