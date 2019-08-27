91f112174401b3db8d190c91d43115998047ec4c
Luiz Felipe Silva <Luizfelipesilva23@hotmail.com>
2019-08-27 01:07:31 +0000

    adding destroy action to the sessions controller with tests

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
+   
     def create
         user = User.find_by(email: session_params[:email])
 
@@ -12,6 +13,12 @@ class Api::V1::SessionsController < ApplicationController
         end
     end
 
+    def destroy
+        user = User.find_by(auth_token: params[:id])
+        user.generate_authentication_token!
+        user.save
+        head 204 #nada a retornar
+    end
 
     private
 
