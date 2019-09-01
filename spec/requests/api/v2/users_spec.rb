07fe9932b74015cf60c0fb2d116dd7b08c7b5d4e
Luiz Felipe Silva <Luizfelipesilva23@hotmail.com>
2019-09-01 01:10:16 +0000

    updating the users api

--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -2,26 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
     let!(:user) { create(:user) }
-    let(:user_id) { user.id }
+    let!(:auth_data) { user.create_new_auth_token }
     let(:headers) do
         {
             'Accept' => 'application/vnd.taskmanager.v2',
             'Content-Type' => Mime[:json].to_s,
-            'Authorization' => user.auth_token 
+            'access-token' => auth_data['access-token'],
+            'uid' => auth_data['uid'],
+            'client' => auth_data['client']  
         }
     end
 
     before{host! "api.taskmanager.test"}
 
-    describe 'GET users/:id' do
-        before do
-            
-            get "/users/#{user_id}", params: {}, headers: headers
+    describe 'GET /auth/validate_token' do
+        
+        before do    
+            get "/auth/validate_token", params: {}, headers: headers
         end
-        context 'when the user existis' do
-            it 'returns the user' do
+        
+        context 'when the requests headers are valid' do
+            it 'returns the user id' do
                 
-                expect(json_body[:data][:id].to_i).to eq(user_id)
+                expect(json_body[:data][:id].to_i).to eq(user.id)
             end
 
             it 'returns status code 200' do
@@ -29,32 +32,33 @@ RSpec.describe 'Users API', type: :request do
             end
         end
 
-        context 'when the user not exist' do
-            let(:user_id) {10000}
+        context 'when the request headers are not valid' do
+            before do    
+                headers['access-token'] = "invalid_token"
+                get "/auth/validate_token", params: {}, headers: headers
+            end
 
-            it 'returns status code 404' do
-                expect(response).to have_http_status(404)
+            it 'returns status code 401' do
+                expect(response).to have_http_status(401)
             end
         end
 
     end
 
-    describe 'Post /users' do
-        before do
-            
-            post '/users', params: {user: user_params}.to_json, headers: headers
+    describe 'Post /auth' do
+        before do  
+            post '/auth', params: user_params.to_json, headers: headers
         end
 
         context 'when the request params are valid' do
             let(:user_params) {FactoryBot.attributes_for(:user)}
 
-            it 'return status code 201' do
-                expect(response).to have_http_status(201)
+            it 'return status code 200' do
+                expect(response).to have_http_status(200)
             end
 
-            it 'returns json data for the created user' do  
-                
-                expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+            it 'returns json data for the created user' do         
+                expect(json_body[:data][:email]).to eq(user_params[:email])
             end
         end
 
@@ -71,11 +75,10 @@ RSpec.describe 'Users API', type: :request do
         end
     end
 
-    describe 'Put /users/:id' do
+    describe 'Put /auth' do
 
-        before do
-            
-            put "/users/#{user_id}", params: {user: user_params}.to_json, headers: headers
+        before do    
+            put "/auth", params: user_params.to_json, headers: headers
         end
 
         context 'when the request params are valid' do
@@ -88,7 +91,7 @@ RSpec.describe 'Users API', type: :request do
             
             it 'returns json data for the uploaded user' do
                 
-                expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+                expect(json_body[:data][:email]).to eq(user_params[:email])
             end
         end
         
@@ -106,14 +109,14 @@ RSpec.describe 'Users API', type: :request do
         end
     end
 
-    describe 'Delete /users/:id' do
+    describe 'Delete /auth' do
         before do
            
-            delete "/users/#{user_id}", params: {}.to_json, headers: headers
+            delete "/auth", params: {}.to_json, headers: headers
         end
 
-        it "returns status code 204" do
-            expect(response).to have_http_status(204)
+        it "returns status code 200" do
+            expect(response).to have_http_status(200)
         end
 
         it 'removes the user from database' do
