6b71576507fe99193ff315af10c940f2a08c7d94
Luiz Felipe Silva <Luizfelipesilva23@hotmail.com>
2019-09-01 03:21:28 +0000

    fixing task serializer

--- app/serializers/api/v2/task_serializer.rb
+++ app/serializers/api/v2/task_serializer.rb
@@ -2,10 +2,8 @@ class Api::V2::TaskSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :done, :deadline, :created_at, :updated_at, :user_id,
               :short_description, :is_late
 
-  
-  
   def short_description
-    object.description[0..40]
+    object.description[0..40] if object.description.present?
   end
 
   def is_late
